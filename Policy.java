d8ca855c2325f207d89dc1d864b2b7e99aa4a845
Mostafa Ahmed <m.a7med82002@gmail.com>
2022-12-20 20:57:59 +0200

    implementation of best fit algorithm, adding new attributes in Partition and Process classes, and modifiy the signature of Policy's function

--- Policy.java
+++ Policy.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public interface Policy {
-    boolean allocateMemory(Process process);
+    boolean allocateMemory(Process process, ArrayList<Partition> partitions);
 }
--- WorstFit.java
+++ WorstFit.java
@@ -1,6 +1,8 @@
+import java.util.ArrayList;
+
 public class WorstFit implements Policy{
     @Override
-    public boolean allocateMemory(Process process) {
+    public boolean allocateMemory(Process process, ArrayList<Partition> partitions) {
         return false;
     }
 }
