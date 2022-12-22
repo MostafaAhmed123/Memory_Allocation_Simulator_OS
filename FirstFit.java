38d700dba4c254871f4eda5a2b1eb7119d6ec010
Bassma Khaled <112968009+bassma-khaled14@users.noreply.github.com>
2022-12-22 13:25:35 +0200

    Update FirstFit.java

--- FirstFit.java
+++ FirstFit.java
@@ -2,7 +2,28 @@ import java.util.ArrayList;
 
 public class FirstFit extends Policy{
     @Override
-    public boolean allocateMemory(Process process, ArrayList<Partition> partitions) {
-        return false;
+        public boolean allocateMemory(Process process, ArrayList<Partition> partitions) {
+
+            int idx = -1;
+
+            for(int i = 0;i<partitions.size();i++){
+
+                if(partitions.get(i).getSize()>=process.getSize()&& partitions.get(i).isFree()){
+                    idx= i;
+                    int fragmentation = partitions.get(i).getSize() - process.getSize();
+
+                }
+            }
+            if(idx!= -1){
+                Partition p = new Partition(partitions.get(idx).getPartitionName() + String.valueOf(idx + 1),partitions.get(idx).getSize()-process.getSize(),true);
+                if(p.getSize()>0)
+                    partitions.add(p);
+                partitions.get(idx).setFree(false);
+                partitions.get(idx).setSize(process.getSize());
+                partitions.get(idx).setProcess(process);
+                return true;
+            }
+            return  false;
+
+        }
     }
-}
