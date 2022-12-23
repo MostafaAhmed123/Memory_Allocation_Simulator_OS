19f2052ba4ecaa9f9fd810c5c1e508141d726030
Mostafa Ahmed <m.a7med82002@gmail.com>
2022-12-23 19:12:21 +0200

    submitted version

--- FirstFit.java
+++ FirstFit.java
@@ -1,29 +1,20 @@
 import java.util.ArrayList;
 
-public class FirstFit extends Policy{
+public class FirstFit extends Policy {
     @Override
-        public boolean allocateMemory(Process process, ArrayList<Partition> partitions) {
-
-            int idx = -1;
-
-            for(int i = 0;i<partitions.size();i++){
-
-                if(partitions.get(i).getSize()>=process.getSize()&& partitions.get(i).isFree()){
-                    idx= i;
-                    int fragmentation = partitions.get(i).getSize() - process.getSize();
-
-                }
-            }
-            if(idx!= -1){
-                Partition p = new Partition(partitions.get(idx).getPartitionName() + String.valueOf(idx + 1),partitions.get(idx).getSize()-process.getSize(),true);
-                if(p.getSize()>0)
-                    partitions.add(p);
-                partitions.get(idx).setFree(false);
-                partitions.get(idx).setSize(process.getSize());
-                partitions.get(idx).setProcess(process);
+    public boolean allocateMemory(Process process, ArrayList<Partition> partitions) {
+        for (int i = 0; i < partitions.size(); i++) {
+            if (partitions.get(i).getSize() >= process.getSize() && partitions.get(i).isFree()) {
+                Partition p = new Partition("Partition " + String.valueOf(Partition.ID),
+                        partitions.get(i).getSize() - process.getSize(), true);
+                if (p.getSize() > 0)
+                    partitions.add(i + 1, p);
+                partitions.get(i).setFree(false);
+                partitions.get(i).setSize(process.getSize());
+                partitions.get(i).setProcess(process);
                 return true;
             }
-            return  false;
-
         }
+        return false;
     }
+}
